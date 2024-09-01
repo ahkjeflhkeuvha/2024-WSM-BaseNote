dbf4adc451082228b1fe9584c0d591d27f919b1f
오지은 <w2317@e-mirim.hs.kr>
2024-09-01 20:42:38 +0900

    DB 초기 세팅

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const user = require('./routes/users');
+app.use('/users', user);
+
+const admin = require('./routes/admins');
+app.use('/admins', admin);
+
+app.listen(port, () => {
+    console.log(`Example app listeing on port ${port}`)
+});
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,13 @@ const router = require('express').Router();
 
 router.post('/login', (req, res) => {
     let {id, pw} = req.body;
+    pool.query('SELECT * FROM client_info WHERE _id = ? AND pw = ?', [id, pw], (err, result) => {
+        if(err){
+            console.error(err.message)
+            res.status(500).json({ message : '로그인 실패' })
+        }
+        else {
+            res.status(200).json({ message : '로그인 성공' })
+        }
+    })
 })
